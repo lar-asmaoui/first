703dfc361e6cbe3da7fd0993ef3dee0f959d3138
lar-asmaoui <asmaouilarbi35@gmail.com>
2023-08-31 10:18:11 +0100

    Update one.sh

--- one.sh
+++ one.sh
@@ -1,11 +1,15 @@
-for i in {1..1000}
+for j in {1..2}
 do
-	now=$(date +%s%3N)
-	echo "{'now':'$now'}" > data.js
-	git add data.js
-	git commit -m "Commit at $now milliseconds"
-	echo "$i/1000"
-	usleep 3000
+	for i in {1..100}
+	do
+		now=$(date +%s%3N)
+		echo "{'now':'$now'}" > data.js
+		git add data.js
+		git commit -m "Commit at $now milliseconds"
+		echo "$i/1000 $j"
+		usleep 1000
+	done
+	git push origin main
+	echo "number of commits: $j/100"
 done
-git push origin main
 echo "[OK] you are the first on commits.top in your country..."
